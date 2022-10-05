a8cf91c5ccd0ab3986d8b29c2c6fbc3d578ac208
anshumannandan <anshuman21xxx034@akgec.ac.in>
2022-10-05 11:28:10 +0530

    added sun and moon

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let sun = document.getElementsByClassName('sun')[0];
+
 function changetime() {
     let bgclr = document.getElementsByTagName('body')[0];
     let bgimg = document.getElementsByClassName('background')[0];
@@ -7,10 +9,19 @@ function changetime() {
         bgclr.style.backgroundColor = "#c8e7ff";
         bgimg.src = "images/night.jpg";
         slider.style.backgroundColor = "#222e48";
+        sun.src = "images/moon.png";
     }
     else {
         bgclr.style.backgroundColor = "#222e48";
         bgimg.src = "images/day.jpg"
         slider.style.backgroundColor = "#c8e7ff";
+        sun.src = "images/sun.png"
     }
+}
+var rot = 0;
+setInterval(animate, 50);
+function animate() {
+    rot += 1
+    sun.style.transform = `rotate(${rot}deg)`;
+    console.log('rotated')
 }
